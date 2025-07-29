5b46577a4cc827fc570eb819bf1197c7dab8050a
Ramiro Silva <ramiromoura92@outlook.com>
2025-07-29 17:04:22 -0300

    update menu names

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import streamlit as st
 
 pages = {
     "Menu Principal": [  # seção
-        st.Page("pages/form.py", title="Formulário"),
-        st.Page("pages/lista_de_itens.py", title="Lista de Itens")
+        st.Page("pages/form.py", title="Confirmar presença"),
+        st.Page("pages/lista_confirmacoes.py", title="Lista de Confirmados"),
+        st.Page("pages/lista_de_itens.py", title="Lista de Mimos"),
+        st.Page("pages/form_add_item.py", title="Adicionar Item"),
     ]
 }
 
