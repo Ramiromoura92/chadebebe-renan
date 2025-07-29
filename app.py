412ebd33afb3dd09d6f1700b21a7434c51c92adf
Ramiro Silva <ramiromoura92@outlook.com>
2025-07-29 16:52:23 -0300

    remove add_itens and confirmados pages from menu

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import streamlit as st
 pages = {
     "Menu Principal": [  # seção
         st.Page("pages/form.py", title="Formulário"),
-        st.Page("pages/lista_confirmacoes.py", title="Lista de Confirmados"),
-        st.Page("pages/lista_de_itens.py", title="Lista de Itens"),
-        st.Page("pages/form_add_item.py", title="Adicionar Item"),
+        st.Page("pages/lista_de_itens.py", title="Lista de Itens")
     ]
 }
 
